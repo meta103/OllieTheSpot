fe4074d68b3157e69b660ee8c257140ae656a1fd
dreazz <agustinsilveira.96@gmail.com>
2019-02-07 21:10:13 +0100

    create user bio edit + working

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const User = require('../models/user');
 
 const router = express.Router();
 
+router.get('/edit', (req, res, next) => {
+  res.render('user/edit');
+  console.log(req.session.currentUser.username);
+});
+router.post('/edit', (req, res, next) => {
+  const currentUserName = req.session.currentUser.username;
+  const { bio } = req.body;
+
+  User.findOneAndUpdate({ username: currentUserName }, { bio })
+    .then(() => {
+      req.session.currentUser.bio = bio;
+      res.redirect('/users/profile');
+    })
+    .catch(next);
+});
 
 /* Put in the buttom just in case */
 router.get('/:username', (req, res, next) => {
