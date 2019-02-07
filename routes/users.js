f87d5203f382595f4899d58d98648798e60871cc
dreazz <agustinsilveira.96@gmail.com>
2019-02-07 19:45:13 +0100

    user profile page create

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 /* Put in the buttom just in case */
 router.get('/:username', (req, res, next) => {
-  res.render('index', { title: 'User route' });
+  res.render('user/profile', { title: 'User route' });
 });
 
 module.exports = router;
