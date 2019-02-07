c73fd1bc0ba511b9081d52248185066f70101c5a
dreazz <agustinsilveira.96@gmail.com>
2019-02-07 12:17:32 +0100

    body parser install

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const bodyParser = require('body-parser');
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -5,8 +6,10 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const expressLayouts = require('express-ejs-layouts');
 
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const spotsRouter = require('./routes/spots');
 
 const app = express();
 
@@ -23,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/spots', spotsRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
