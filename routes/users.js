7ab63e55d14dc76532b85aa634f777eeeb66181e
dreazz <agustinsilveira.96@gmail.com>
2019-02-07 17:59:06 +0100

    navbar logout create

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-/* GET home page. */
+
+/* Put in the buttom just in case */
 router.get('/:username', (req, res, next) => {
   res.render('index', { title: 'User route' });
 });
