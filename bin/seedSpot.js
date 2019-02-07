2e07c7ddef376c0fe80b2e82462d3170eb58af79
Marcos Tagliabue <marcostagliabue103@gmail.com>
2019-02-07 15:31:05 +0100

    models are finished

---
+++ bin/seedSpot.js
@@ -0,0 +1,26 @@
+
+
+const Spot = require('../models/spot');
+
+
+
+
+const spotArray = [
+  {
+    owner: 'Arnold Schwarzenegger',
+    name: 'Actor',
+    description: 'I’ll be back',
+    image: '',
+    location: 'Barcelona',
+  },
+  {
+    name: 'Britney Spears',
+    occupation: 'Singer',
+    catchPhrase: 'The older I get the more I understand Britney’s 2007 meltdown',
+  },
+  {
+    name: 'Justin Timberlake',
+    occupation: 'Singer',
+    catchPhrase: 'I’ll know when I know. And until I know, I don’t know',
+  },
+];
