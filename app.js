0be1e3c17927caaf997ddcffe0f1b64295e6242c
dreazz <agustinsilveira.96@gmail.com>
2019-02-07 12:38:55 +0100

    start kit done

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const expressLayouts = require('express-ejs-layouts');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+mongoose
+  .connect('mongodb://localhost/olliethespot', { useNewUrlParser: true })
+  .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+  })
+  .catch((err) => {
+    console.error('Error connecting to mongo', err);
+  });
 
 
 const indexRouter = require('./routes/index');
@@ -18,11 +29,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('layout', 'layouts/layout');
 
+// Middleware setup
 app.use(logger('dev'));
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressLayouts);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
+  res.render('index', { title: 'User route' });
 });
 
 module.exports = router;
