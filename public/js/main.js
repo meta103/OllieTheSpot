ef679ad4a9773d9fb1e6e476ac0cb129806aecbc
dreazz <agustinsilveira.96@gmail.com>
2019-02-11 19:03:28 +0100

    signup toggle working

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,9 @@
-const access = document.querySelector()
-;
+const accessSignUp = document.querySelector('#sign-up');
+const accessLogIn = document.querySelector('#login');
+const signUpButton = document.querySelector('.access-button');
+
+signUpButton.addEventListener('click', () => {
+  accessSignUp.classList.toggle('hide');
+  accessLogIn.classList.toggle('hide');
+  console.log('hi');
+});
