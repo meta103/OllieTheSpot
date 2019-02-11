60a116796fcde51d2fed22e6367b6570ef152a34
dreazz <agustinsilveira.96@gmail.com>
2019-02-11 19:43:50 +0100

    dom on access button

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,18 @@
 const accessSignUp = document.querySelector('#sign-up');
 const accessLogIn = document.querySelector('#login');
 const signUpButton = document.querySelector('.access-button');
+const accessText = document.querySelector('.access-text');
+
 
 signUpButton.addEventListener('click', () => {
   accessSignUp.classList.toggle('hide');
   accessLogIn.classList.toggle('hide');
+  if (signUpButton.innerHTML === 'Log in') {
+    accessText.innerHTML = 'Don\'t have an account yet?';
+    signUpButton.innerHTML = 'Sign up';
+  } else {
+    accessText.innerHTML = 'Do you have an account?';
+    signUpButton.innerHTML = 'Log in';
+  }
   console.log('hi');
 });
